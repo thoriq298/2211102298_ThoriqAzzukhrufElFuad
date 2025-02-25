daacd7e7d11013b6405eddadb87306f575f4b176
thoriq298 <thoriqazzukhruf03@gmail.com>
2025-02-25 21:59:55 +0700

    praktikum 1

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Hello World",
+      home: const MyHomePage(title: "Thoriq Azzukhruf El Fuad"),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Center(child: Text('2211102298')),
+    );
+  }
+}
